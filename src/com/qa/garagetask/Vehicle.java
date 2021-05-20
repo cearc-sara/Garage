6b2a2135ed8a07dd66f5d0503e25fc392d949dea
Sara Cearc <scearc930@gmail.com>
2021-05-20 16:04:30 -0400

    added different classes off of vehicle

--- src/com/qa/garagetask/Vehicle.java
+++ src/com/qa/garagetask/Vehicle.java
@@ -1,5 +1,42 @@
 package com.qa.garagetask;
 
-public class Vehicle {
+public abstract class Vehicle {
+	// Attribute
+	private boolean hasTires;
+	private int doors;
+	private String manufacturer;
+	private String model;
+	
+	// Constructor
+	public Vehicle(String manufacturer, String model, int doors) {
+		this.manufacturer = manufacturer;
+		this.model = model;
+		this.doors = doors;
+		this.hasTires = true;
+		
+	}
+	
+	// Methods
+	
+	
+	public boolean hasTires() {
+		return hasTires;
+	}
+	
+	public int getDoors() {
+		return doors;
+	}
+	
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	
+	
+
+	
 
 }
